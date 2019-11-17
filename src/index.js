3a6813ee9b4fc5290fc86549126c7b24fabad514
jelee603 <jelee603@gmail.com>
2019-11-17 23:43:15 +0900

    tutorial 1~6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import Clock from './Clock'
+import Toggle from './Toggle'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+//   ReactDOM.render(element, document.getElementById('root'))
+// }
+
+// setInterval(tick, 1000);
+
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>
+// }
+
+// const element = <Welcome name="Sara" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// )
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}</h2>
+//     </div>
+//   )
+// }
+// function tick() {
+//   ReactDOM.render(
+//     <Clock date={new Date()} />,
+//     document.getElementById('root')
+//   )
+// }
+// setInterval(tick, 1000)
+
+ReactDOM.render(
+  <Toggle/>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
